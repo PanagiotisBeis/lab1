eb6a760657155ffb316fdb3666cf9a19d64f025b
Panagiotis Beis <beisp@yahoo.com>
2024-02-26 11:54:56 +0200

    added cookies

--- lab1.py
+++ lab1.py
@@ -15,3 +15,16 @@ url = 'http://google.com/'  # προσδιορισμός του url
 with requests.get(url) as response:  # το αντικείμενο response
     html = response.text
     more(html)
+
+    print(f"Website headers are {url} \n, {response.headers} \n\n")
+
+    server = response.headers.get('Server')
+
+    if server:
+        print(f"The server is {server}")
+    else:
+        print("Server not found")
+
+    cookies = response.cookies.get_dict()
+
+    print (cookies)
